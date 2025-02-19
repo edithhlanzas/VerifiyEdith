c9236e95456c0340f428101708f881e463e5d3f9
EDITHELIZABETH <est.ehernandez|0301@uml.edu.ni>
2025-02-18 22:11:13 -0600

    configurando vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,20 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "path";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(() => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
+    test: {
+      globals: true,
+      environment: "jsdom",
 
-  },
+      
+    },
+  };
 });
-
-
-
